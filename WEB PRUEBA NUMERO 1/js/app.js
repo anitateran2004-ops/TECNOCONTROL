2c3a90053edeaee7ef8f75f6700d8a96ac58e268
anitateran2004-ops <anitateran2004@gmail.com>
2026-04-14 13:17:14 -0300

    Add files via upload

---
+++ WEB PRUEBA NUMERO 1/js/app.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function cambiarSlide() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}
+setInterval(cambiarSlide, 4000);
